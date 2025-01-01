7d97025243f3ea79b1d7d0b73428bb743d8c0623
Beboto <bebotoghosh10@gmail.com>
2025-01-01 11:35:45 +0530

    changes made in config.js files

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,4 @@
 const LocalURL = "http://localhost:8000";
-// const ProductionURL = "https://chess-game-bslt.onrender.com";
 const ProductionURL = "https://chess-game-48nu.onrender.com";
 
 const BackendURL = process.env.NODE_ENV === 'production' ? ProductionURL : LocalURL;
